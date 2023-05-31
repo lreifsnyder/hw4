f9ab32a4a0b92f5b1e0589d11d1cbc90431ade18
Laura Reifsnyder <laura.reifsnyder@kellogg.northwestern.edu>
2023-05-31 22:46:08 +0000

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if @user["password"] == params["password"]
+    redirect_to "/places/index"
+      else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
